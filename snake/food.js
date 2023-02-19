669450894a8dce4df386012791c04ecdfd8c1a7e
Alexandre Junior <121458389+AlexandreJr16@users.noreply.github.com>
2023-02-19 16:17:26 -0300

    Add files via upload

---
+++ snake/food.js
@@ -0,0 +1,34 @@
+import { onSnake, expandSnake } from "./snake.js";
+import { randomGridPosition } from "./grid.js";
+
+let food = {
+  x: 0,
+  y: 0,
+};
+
+const EXPASION_RATE = 1;
+
+food = getRandomFoodPosition();
+
+export function update() {
+  if (onSnake(food)) {
+    expandSnake(EXPASION_RATE);
+    food = getRandomFoodPosition();
+  }
+}
+
+export function draw(gameBoard) {
+  const foodElement = document.createElement("div");
+  foodElement.style.gridRowStart = food.y;
+  foodElement.style.gridColumnStart = food.x;
+  foodElement.classList.add("food");
+  gameBoard.appendChild(foodElement);
+}
+function getRandomFoodPosition() {
+  let newFoodPosition;
+
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
+    newFoodPosition = randomGridPosition();
+  }
+  return newFoodPosition;
+}
---
+++ snake/snake.js
@@ -0,0 +1,40 @@
+export const SNAKE_SPEED = 4;
+import { getInputDirection } from "./input.js";
+
+const snakeBody = [{ x: 11, y: 11 }];
+
+let newSegments = 0;
+
+export function update() {
+  addSegments();
+  const inputDirection = getInputDirection();
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] };
+  }
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
+}
+
+export function draw(gameBoard) {
+  snakeBody.forEach((segment) => {
+    const snakeElement = document.createElement("div");
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
+    snakeElement.classList.add("snake");
+    gameBoard.appendChild(snakeElement);
+  });
+}
+export function expandSnake(amount) {
+  newSegments += amount;
+}
+export function addSegments() {
+  for (let i = 0; i < newSegments; i++) {
+    snakeBody.push({ ...snakeBody[snakeBody.length - 1] });
+  }
+  newSegments = 0;
+}
+export function onSnake(position) {
+  return snakeBody.some((segment, index) => {
+    return position.x == segment.x && position.y == segment.y;
+  });
+}
